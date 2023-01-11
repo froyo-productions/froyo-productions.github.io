56942da88c9fd5371d4924bb9b5e51014f921c91
tyler-b-williams <silverzx713666@gmail.com>
2023-01-10 19:06:33 -0500

    copy to clipboard function works decently

--- square_loadin.js
+++ square_loadin.js
@@ -1,15 +1,15 @@
 var width = window.innerWidth;
 var height = window.innerHeight;
 
-console.log(width, height);
-console.log(typeof(height));
+// console.log(width, height);
+// console.log(typeof(height));
 
 // have no choice but to make full square that someone scales with height
 // var rowSquares = 3;
 // var colSquares = 2;
 var squareCount = Math.round(Math.max(height, width) / 33);
 // var squareCount = Math.round((height) / 35);
-console.log(squareCount);
+// console.log(squareCount);
 var genUL = $(document.createElement('ul'));
 genUL.attr('style', '--square-count:' + squareCount)
     // $(genUL).addClass('row-cols');
